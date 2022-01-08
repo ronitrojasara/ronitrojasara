430cfafd9ff29785de04d7a4198947e9dd89318d
ronitrojasara <84675961+ronitrojasara@users.noreply.github.com>
2022-01-08 09:34:19 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,4 @@
+echo "welcome to my repository" | lolcat -a
+echo "3 files" | lolcat -as 100
+echo "thank you for visiting" | lolcat -as 100
+echo "-------[Instagram.com/ronitrojasara]-------" |  lolcat -a
